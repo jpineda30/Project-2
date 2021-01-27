04fa3ec56f40c29c1d4c68f8f208a82175d7a9f7
¨Mario Castro¨ <mnc634@utexas.edu>
2021-01-26 21:16:25 -0600

    fixing issues with branch

--- public/js/doctapp.js
+++ public/js/doctapp.js
@@ -24,59 +24,10 @@ $(function(){
    
    });
 
-   console.log(patient);
-
-   $("#myModal").css("display","none");
-});
-
- $(".closeEdit, #cancelEdit, #savePatientEdit").on("click",function(){
-    $("#editModal").css("display","none");
- });
-
- $("#exitView, .closeView").on("click",function(){
-    $("#viewModal").css("display","none");
- });
-
- $("#delete-icon").on("click",function(){
-    
-    $("#delete-prompt").css("display","block");
-
-});
-
-$("#cancelDel, .closeDeletion, #deleteBtn").on("click",function(){
-    $("#delete-prompt").css("display","none");
-});
-
-// events
-
-$("#menu-appointments").on("click", ()=>{
-
-   window.location.replace("/appointments");
-
-});
+   $("#menu-services").on("click", ()=>{
    
-$("#menu-calendar").on("click", ()=>{
- 
-   window.location.replace("/calendar");
+      window.location.replace("/services");
+   
+   });
 
-});
-
-$("#menu-profile").on("click", ()=>{
-
-   window.location.replace("/profile");
-
-});
-
-$("#menu-patients").on("click", ()=>{
-
-   window.location.replace("/patients");
-
-});
-
-$("#menu-treatments").on("click", ()=>{
-
-   window.location.replace("/treatments");
-
-});
-
-
+})
